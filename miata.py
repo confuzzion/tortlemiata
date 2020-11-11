dc01d5c1ea91d4f3922f95837ae98087625065ad
confuzzion <60537768+confuzzion@users.noreply.github.com>
2020-11-10 21:56:23 -0800

    Added first working version of code,
    can now get rpm in the car. working next on a data rain for rest of live data.

--- miata.py
+++ miata.py
@@ -1,10 +1,9 @@
 import obd
 
-connection = obd.OBD() # auto-connects to USB or RF port
+connection = obd.OBD("/dev/rfcomm0") # auto-connects to USB or RF port
 
-cmd = obd.commands.SPEED # select an OBD command (sensor)
-
-response = connection.query(cmd) # send the command, and parse the response
-
-print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
+while(True): #prints RPM over and over again
+  #cmd = obd.commands.RPM
+  #response = connection.query(cmd)
+  #print(response.value)
+  print(connection.query(obd.commands.RPM).value)
